27bc8f1815345a962a180d709e1788aa80d99c4c
Connell <connellsharp@gmail.com>
2021-03-03 20:34:10 +0000

    Initial ramblings

---
+++ src/FluentArrangement/Fixture.cs
@@ -0,0 +1,43 @@
+namespace FluentArrangement
+{
+    internal class Fixture
+    {
+        public T Create<T>(string name = null)
+        {
+
+        }
+    }
+
+    public interface IFactory
+    {
+        CreateResponse Create(CreateRequest request);
+    }
+
+    internal class AggregateFactory : IFactory
+    {
+        public CreateResponse Create(CreateRequest request)
+        {
+            
+        }
+    }
+
+    internal class CtorAndPropsFactory : IFactory
+    {
+        public CreateResponse Create(CreateRequest request)
+        {
+            // call ctor and set properties from fixture
+
+            return new CreateResponse(obj);
+        }
+    }
+
+    internal class MockEverythingFactory : IFactory
+    {
+        public CreateResponse Create(CreateRequest request)
+        {
+            // create mock and configure props and methods to return from fixture
+
+            return new CreateResponse(null);
+        }
+    }
+}
