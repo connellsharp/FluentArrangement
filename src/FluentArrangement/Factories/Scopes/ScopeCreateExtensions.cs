9ee5c2352bc041bbf0506664bff11ccf64edf0ef
Connell <connellsharp@gmail.com>
2021-08-22 19:11:57 +0100

    Flip CreateObject generic extensions

--- src/FluentArrangement/Factories/Scopes/ScopeCreateExtensions.cs
+++ src/FluentArrangement/Factories/Scopes/ScopeCreateExtensions.cs
@@ -4,18 +4,18 @@ namespace FluentArrangement
 {
     internal static class ScopeCreateExtensions
     {
-        internal static T? CreateObject<T>(this IScope scope, ICreateRequest request)
+        internal static object? CreateObject(this IScope scope, ICreateRequest request)
         {
             var response = scope.Create(request, scope);
 
             if(!response.HasCreated)
                 throw request.GetNotCreatedException();
 
-            return (T?)response.CreatedObject;
+            return response.CreatedObject;
         }
 
-        internal static object? CreateObject(this IScope scope, ICreateRequest request)
-            => scope.CreateObject<object>(request);
+        internal static object? CreateObject<T>(this IScope scope, ICreateRequest request)
+            => (T?)scope.CreateObject(request);
 
         internal static object? CreateObjectFromType(this IScope scope, Type type)
             => scope.CreateObject(new CreateTypeRequest(type));
